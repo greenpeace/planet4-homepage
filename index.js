ae341f22924ef2599182aa040104ca0faa3e4d93
Nikos Roussos <nikos@roussos.cc>
2021-03-10 11:51:37 +0200

    Exclude search from rewrite

--- index.js
+++ index.js
@@ -8,14 +8,15 @@ addEventListener('fetch', event => {
 async function handle(request) {
   const requestURL = new URL(request.url);
   const path = requestURL.pathname.replace(/\/+$/, '');
+  const search = requestURL.search;
 
-  if (requestURL.search.indexOf('&preview=true') == -1) {
-    if (!path || path == '/') {
+  if (path == '/features') {
+    return await fetch('https://storage.googleapis.com/planet4-homepage/features/index.html');
+  }
+
+  if (!path || path == '/') {
+    if (!search.includes('?s=') && !search.includes('&preview=true')) {
       return await fetch('https://storage.googleapis.com/planet4-homepage/index.html');
-    }
-
-    if (path == '/features') {
-      return await fetch('https://storage.googleapis.com/planet4-homepage/features/index.html');
     }
   }
 
